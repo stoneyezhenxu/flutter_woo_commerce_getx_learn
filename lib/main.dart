2eefe1071bf92243f882b7d74d020eb39251ce9e
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-10-09 19:36:08 +0100

    add flutter_screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'common/index.dart';
 import 'global.dart';
 import 'package:get/get.dart';
@@ -14,22 +15,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: RouteNames.systemSplash,
-      // initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+    return ScreenUtilInit(
+      designSize: const Size(414, 896),
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
+          initialRoute: RouteNames.systemSplash,
+          // initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
 
-      // 样式
-      theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
+          // 样式
+          theme: ConfigService.to.isDarkModel ? AppTheme.dark : AppTheme.light,
 
-      // 多语言
-      translations: Translation(), // 词典
-      localizationsDelegates: Translation.localizationsDelegates, // 代理
-      supportedLocales: Translation.supportedLocales, // 支持的语言种类
-      locale: ConfigService.to.locale, // 当前语言种类
-      fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+          // 多语言
+          translations: Translation(), // 词典
+          localizationsDelegates: Translation.localizationsDelegates, // 代理
+          supportedLocales: Translation.supportedLocales, // 支持的语言种类
+          locale: ConfigService.to.locale, // 当前语言种类
+          fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
